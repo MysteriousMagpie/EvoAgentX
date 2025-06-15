575952e7891e9de7d9f65bd6c8532888718d8bb4
MysteriousMagpie <malachisellsproduce@gmail.com>
2025-06-15 16:03:27 -0700

    Limit coverage scope to utils and optimizers

---
+++ tests/test_utils_smoke.py
@@ -0,0 +1,36 @@
+import json
+import types
+import sys
+
+from evoagentx.utils.utils import safe_remove, generate_dynamic_class_name, normalize_text
+from evoagentx.utils.sanitize import syntax_check
+import evoagentx.utils.aflow_utils.data_utils as du
+du.test_case_2_test_function.__test__ = False
+
+# import factory with minimal storages stubs to avoid circular imports
+cfg_mod = types.ModuleType("evoagentx.storages.storages_config")
+class DBConfig: ...
+class VectorStoreConfig: ...
+cfg_mod.DBConfig = DBConfig
+cfg_mod.VectorStoreConfig = VectorStoreConfig
+sys.modules.setdefault("evoagentx.storages", types.ModuleType("evoagentx.storages"))
+sys.modules["evoagentx.storages.storages_config"] = cfg_mod
+from evoagentx.utils.factory import load_class
+
+
+def test_utils_smoke():
+    lst = [1, 2, 3]
+    safe_remove(lst, 2)
+    assert lst == [1, 3]
+
+    assert generate_dynamic_class_name("hello world") == "HelloWorld"
+    assert normalize_text("The_quick") == "quick"
+
+    cls = load_class("json.JSONDecoder")
+    assert cls is json.JSONDecoder
+
+    assert syntax_check("def f():\n    return 1")
+    assert not syntax_check("def f(:\n    pass")
+
+    snippet = du.test_case_2_test_function("def foo(): pass", "assert True", "foo()")
+    assert "def check" in snippet
